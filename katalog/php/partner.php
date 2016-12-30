e5c0d47df070e63d2d2ee06549d1a9fdca6d1fc2
shadaxv <aleksandern1116@gmail.com>
2016-12-30 21:33:43 +0100

    Add files via upload

---
+++ katalog/php/partner.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+
+<html lang=pl>
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Katalog biblioteki online</title>
+		<meta name="description" content="Najlepsza biblioteka internetowa we wszechświecie">
+		<meta name="keywords" content="biblioteka, wypożyczalnia, książek, książki, książka, lektura, powieść, tom, rodział itd. XD">
+		<meta name="author" content="Aleksander Niedźwiedź, Dominik Borowiec, Bartosz Łodej">
+		<meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
+		<link rel="stylesheet" href="../css/normalize.css?v1.0">
+		<link rel="stylesheet" href="../css/main.css?v1.11">
+		<link rel="stylesheet" href="../css/fontello.css?v1.0">
+		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&subset=latin-ext" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,600&subset=latin-ext" rel="stylesheet">
+		<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+
+		<!-- Chrome, Firefox OS, Opera and Vivaldi bar color -->
+		<meta name="theme-color" content="#0062a6">
+		<!-- Windows Phone bar color -->
+		<meta name="msapplication-navbutton-color" content="#0062a6">
+		<!-- iOS Safari bar color -->
+		<meta name="apple-mobile-web-app-status-bar-style" content="#0062a6">
+
+		<!-- IKONY -->
+		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="icon/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="icon/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="icon/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="icon/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon-precomposed" sizes="60x60" href="icon/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon-precomposed" sizes="120x120" href="icon/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon-precomposed" sizes="76x76" href="icon/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon-precomposed" sizes="152x152" href="icon/apple-touch-icon-152x152.png">
+		<link rel="icon" type="image/png" href="icon/favicon-196x196.png" sizes="196x196">
+		<link rel="icon" type="image/png" href="icon/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="icon/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="icon/favicon-16x16.png" sizes="16x16">
+		<link rel="icon" type="image/png" href="icon/favicon-128.png" sizes="128x128">
+		<meta name="application-name" content="&nbsp;">
+		<meta name="msapplication-TileColor" content="#FFFFFF">
+		<meta name="msapplication-TileImage" content="icon/mstile-144x144.png">
+		<meta name="msapplication-square70x70logo" content="icon/mstile-70x70.png">
+		<meta name="msapplication-square150x150logo" content="icon/mstile-150x150.png">
+		<meta name="msapplication-wide310x150logo" content="icon/mstile-310x150.png">
+		<meta name="msapplication-square310x310logo" content="icon/mstile-310x310.png">
+		<!-- IKONY -->
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+	</head>
+
+	<body>
+
+		<div class="container">
+
+				<header>
+
+					<div class="account-nav">
+
+							<div class="header-left">
+								<div class="social-btn">
+									<div style="float:left; padding: 2px;"><a href="http://facebook.com" target="_blank"><i class="icon-facebook"></i></a></div>
+									<div style="float:left; padding: 2px;"><a href="http://twitter.com" target="_blank"><i class="icon-twitter"></i></a></div>
+									<div style="float:left; padding: 2px;"><a href="http://plus.google.com" target="_blank"><i class="icon-gplus"></i></a></div>
+									<div style="float:left; padding: 2px;"><a href="http://linkedin.com" target="_blank"><i class="icon-linkedin"></i></a></div>
+									<div style="clear; both"></div>
+								</div>
+								<div class="phone-number">
+									<span>Potrzebujesz pomocy?&nbsp;</span> <span>Zadzwoń! 123 456 789</span>
+								</div>
+								<div style="clear; both"></div>
+							</div>
+
+							<div class="header-right">
+								<div class="account-btn">
+									<ul class="account-menu">
+										<li style="display: none;"><a href="">Witaj $usernname</a></li>
+										<li><a href="" style="border: 2px solid #fc3; border-radius: 5px; font-weight: 600;">ZAREJESTRUJ SIĘ</a></li>
+										<li><a href="">ZALOGUJ SIĘ</a></li>
+									</ul>
+									<div style="clear; both"></div>
+								</div>
+								<div style="clear; both"></div>
+							</div>
+							<div style="clear; both"></div>
+					</div>
+					<div style="clear; both"></div>
+
+					<div class="search-nav">
+						<div class="logo-div"><a href="#"><div class="logo"></div><span style="padding-left: 5px;">&nbsp;BIBLIOTEKA&nbsp;</span><span>ON-LINE</span></a></div>
+						<div class="search-input"><input type="text" placeholder="Wpisz tytuł, nazwę autora, rok wydania lub wydawnictwo"><a href="#"></a></div>
+						<div class="order">order</div>
+					</div>
+					<div style="clear: both"></div>
+
+					<div class="menu-div">
+						<ul class="menu">
+						  <li class="hvr-shutter-out-vertical"><a href="../index.php"><div>HOME</div></a></li>
+						  <li class="hvr-shutter-out-vertical"><a href="newsy.php"><div>NEWSY</div></a></li>
+						  <li class="hvr-shutter-out-vertical"><a href="kontakt.php"><div>KONTAKT</div></a></li>
+						  <li class="hvr-shutter-out-vertical"><a href="#" style="background-color: #fc3; color: white;"><div>WSPÓŁPRACA</div></a></li>
+						  <li class="hvr-shutter-out-vertical"><a href="onas.php"><div>O NAS</div></a></li>
+						  <li class="hvr-shutter-out-vertical"><a href="sklep.php"><div>SKLEP</div></a></li>
+						  <li class="hvr-shutter-out-vertical"><a href="katalog.php"><div>KATALOG</div></a></li>
+						</ul>
+					</div>
+					<div style="clear; both"></div>
+
+				</header>
+
+				<div class="main-slider">
+					<div class="slider-shadow">
+            <div class="slider-text">
+    					“Dobra książka to rodzaj alkoholu - też idzie do głowy”<span style="font-size: 12px; font-weight: 600;"> XD</span><br>
+    					<span style="font-weight: 400; font-size: 24px">Magdalena Samozwaniec</span>
+              <ul class="slider-button">
+                <li><a href="">ZAŁÓŻ KONTO</a></li>
+              </ul>
+            </div>
+					</div>
+				</div>
+
+        container
+
+		</div>
+
+		<div class="noentry"><div class="noentrymsg">Nie obsługujemy urządzeń poniżej 290px <br> Kup sobie normalny telefon biedaku XDD</div></div>
+
+	</body>
+
+</html>
